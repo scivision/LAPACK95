4dc1e0fe9c8e63ededfef60c0064134d56d0b3e5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-03-31 21:31:54 -0400

    cmake/find template

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,4 @@
 
-if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
-
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  list(APPEND FFLAGS -std=legacy -fno-trapping-math -march=native)
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL PGI)
-
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL Flang)
-
-elseif(CMAKE_Fortran_COMPILER_ID STREQUAL NAG)
-  # https://www.nag.co.uk/nagware/np/r62_doc/manual/compiler_2_4.html#OPTIONS
+if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+  string(APPEND CMAKE_Fortran_FLAGS " -std=legacy -fno-trapping-math -mtune=native")
 endif()
